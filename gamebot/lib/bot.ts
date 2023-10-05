8d78933004cd480ca7820bf834dafe772bf0134a
Zijian Zhang <futrime@outlook.com>
2023-10-05 18:30:26 +0800

    feat(gamebot): add skeleton for ts

---
+++ gamebot/lib/bot.ts
@@ -0,0 +1,73 @@
+//@ts-check
+'use strict';
+
+import consola from 'consola';
+import minecraftData from 'minecraft-data';
+import mineflayer from 'mineflayer';
+import collectblock from 'mineflayer-collectblock';
+import pathfinder from 'mineflayer-pathfinder';
+import pvp from 'mineflayer-pvp';
+
+/**
+ * Represents a bot that can play Minecraft.
+ */
+export class Bot {
+  private wrappedMineflayerBot: mineflayer.Bot;
+  private readonly mcdata: minecraftData.IndexedData;
+
+  /**
+   * @param host The server host.
+   * @param port The server port.
+   * @param username The username.
+   * @param version The Minecraft version.
+   */
+  constructor(host: string, port: number, username: string, version: string) {
+    this.wrappedMineflayerBot =
+        this.createMineflayerBot(host, port, username, version);
+    this.mcdata = minecraftData(version);
+  }
+
+  get mineflayerBot(): mineflayer.Bot {
+    return this.wrappedMineflayerBot;
+  }
+
+  /**
+   * Initializes the mineflayer bot.
+   * @param host The server host.
+   * @param port The server port.
+   * @param username The username.
+   * @param version The Minecraft version.
+   * @returns The mineflayer bot.
+   */
+  private createMineflayerBot(
+      host: string, port: number, username: string,
+      version: string): mineflayer.Bot {
+    const bot = mineflayer.createBot({
+      auth: 'offline',
+      host: host,
+      port: port,
+      username: username,
+      version: version,
+    });
+
+    bot.loadPlugin(collectblock.plugin);
+    bot.loadPlugin(pathfinder.pathfinder);
+    bot.loadPlugin(pvp.plugin);
+
+    bot.on('end', (async () => {
+             consola.log(`bot ended, restarting in 5 seconds...`);
+             await new Promise((resolve) => setTimeout(resolve, 5000));
+
+             this.wrappedMineflayerBot =
+                 this.createMineflayerBot(host, port, username, version);
+           }));
+    bot.on('error', (error) => {
+      consola.error(`bot error: ${error.message}`);
+    });
+    bot.on('login', () => {
+      consola.success('bot logged in');
+    });
+
+    return bot;
+  }
+}
